a15103f35a7f953854fe210f9f77ec99ba81d916
Abeltosin <143393032+Abeltosin@users.noreply.github.com>
2024-05-02 23:48:10 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,31 +1,46 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-
 const server = jsonServer.create()
+const router = jsonServer.router('db.json') // Replace with your db.json path
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+server.use(jsonServer.defaults())
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+// User Endpoints (all assuming /users as the base URL)
+server.get('/users', (req, res) => {
+  // GET all users
+  res.json(router.db.get('users').value())
 })
 
-// Export the Server API
-module.exports = server
+server.post('/users', (req, res) => {
+  // POST a new user (assign a unique ID)
+  const newUser = { ...req.body, id: Math.random().toString(36).substring(2, 15) }
+  router.db.get('users').push(newUser).write()
+  res.json(newUser)
+})
+
+server.put('/users/:id', (req, res) => {
+  // PUT update user data
+  const { id } = req.params;
+  const userIndex = router.db.get('users').findIndex(user => user.id === id)
+  if (userIndex !== -1) {
+    router.db.get('users').splice(userIndex, 1, req.body).write()
+    res.json(req.body)
+  } else {
+    res.sendStatus(404) // User not found
+  }
+})
+
+server.delete('/users/:id', (req, res) => {
+  // DELETE a user
+  const { id } = req.params;
+  const userIndex = router.db.get('users').findIndex(user => user.id === id)
+  if (userIndex !== -1) {
+    router.db.get('users').splice(userIndex, 1).write()
+    res.sendStatus(204) // No content to return on successful delete
+  } else {
+    res.sendStatus(404) // User not found
+  }
+})
+
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
